f61034692f08f1ef806fafd910b8dc3f2437904a
syedaftab-dev <syed.md.aftab.2027@gmail.com>
2026-05-19 19:30:37 +0530

    restructured project into frontend and backend

---
+++ backend/src/index.js
@@ -0,0 +1,42 @@
+import AgentAPI from "apminsight";
+AgentAPI.config();
+
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import {matchRouter} from "./routes/matches.js";
+import {attachWebSocketServer} from "./ws/server.js";
+import {securityMiddleware} from "./arcjet.js";
+import {commentaryRouter} from "./routes/commentary.js";
+
+const PORT = Number(process.env.PORT || 8000);
+const HOST = process.env.HOST || '0.0.0.0';
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express server!');
+});
+
+app.use(securityMiddleware());
+
+app.use('/matches', matchRouter);
+app.use('/matches/:id/commentary', commentaryRouter);
+
+const { broadcastMatchCreated, broadcastCommentary, broadcastScoreUpdate } = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+app.locals.broadcastCommentary = broadcastCommentary;
+app.locals.broadcastScoreUpdate = broadcastScoreUpdate;
+
+server.listen(PORT, HOST, () => {
+    const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+
+    console.log(`Server is running on ${baseUrl}`);
+    console.log(`WebSocket Server is running on ${baseUrl.replace('http', 'ws')}/ws`);
+});
+
+
